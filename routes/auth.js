8ab9a71b72c5cb87061ef7241a75a69f62d611e5
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-05 10:26:50 -0700

    register route bug fix, parameters passed in object not as comma seperated values. -credit Robin

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,15 @@ const Router = require("express").Router;
 const router = new Router();
 const User = require("../models/user");
 const { SECRET_KEY } = require("../config");
+const { BadRequestError } = require("../expressError");
+const jwt = require("jsonwebtoken");
+
 
 /** POST /login: {username, password} => {token} */
-router.post("/login", async function (req, res, next) {
+router.post("/login", async function (req, res) {
   const { username, password } = req.body;
-
+  //User.authenticate
+  //updateLoginTimestamp?
   const authResult = await authenticate(username, password);
 
   if (authResult === true) {
@@ -20,16 +24,36 @@ router.post("/login", async function (req, res, next) {
 
 });
 
-// if (user) {
-//   if (await bcrypt.compare(password, user.password) === true) {
-//     const token = jwt.sign({ username }, SECRET_KEY);
-//     return res.json({ token });
-//   }
-
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post("/register", async function (req,res){
+  const {username, password, first_name, last_name, phone} = req.body;
+  console.log('^^^^^^^^',username, password, first_name, last_name, phone);
+
+  // TODO: Validate inputs?
+  // try{
+    const newUser = await User.register({
+                                        username,
+                                        password,
+                                        first_name,
+                                        last_name,
+                                        phone,
+  });
+    console.log('******************',newUser);
+  // } catch (error){
+  //   throw new BadRequestError("Invalid Username");
+  // }
+
+  //TODO: Logs In Anything needed?
+  const token = jwt.sign({ username }, SECRET_KEY);
+  console.log('#########################',token);
+  return res.json({ token });
+})
+
+
 
 module.exports = router;
