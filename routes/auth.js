efd7ed0ce09463eade11ecb0c38aa125e2628a07
Clair-Hawkes <hawkesj7@gmail.com>
2022-07-05 10:54:08 -0700

    Add update loginTime to Login route

--- routes/auth.js
+++ routes/auth.js
@@ -4,16 +4,20 @@ const Router = require("express").Router;
 const router = new Router();
 const User = require("../models/user");
 const { SECRET_KEY } = require("../config");
-const { BadRequestError } = require("../expressError");
+const { BadRequestError,UnauthorizedError } = require("../expressError");
 const jwt = require("jsonwebtoken");
 
 
 /** POST /login: {username, password} => {token} */
 router.post("/login", async function (req, res) {
   const { username, password } = req.body;
-  //User.authenticate
-  //updateLoginTimestamp?
-  const authResult = await authenticate(username, password);
+
+  const authResult = await User.authenticate(username, password);
+  // const newLoginTime = await User.updateLoginTimestamp(username);
+  // TODO: Variable vs. no variable?
+  await User.updateLoginTimestamp(username);
+
+
 
   if (authResult === true) {
     const token = jwt.sign({ username }, SECRET_KEY);
@@ -32,10 +36,8 @@ router.post("/login", async function (req, res) {
 
 router.post("/register", async function (req,res){
   const {username, password, first_name, last_name, phone} = req.body;
-  console.log('^^^^^^^^',username, password, first_name, last_name, phone);
 
-  // TODO: Validate inputs?
-  // try{
+  try{
     const newUser = await User.register({
                                         username,
                                         password,
@@ -43,14 +45,12 @@ router.post("/register", async function (req,res){
                                         last_name,
                                         phone,
   });
-    console.log('******************',newUser);
-  // } catch (error){
-  //   throw new BadRequestError("Invalid Username");
-  // }
+  } catch (error){
+    throw new BadRequestError("Invalid/Duplicate Username");
+  }
 
   //TODO: Logs In Anything needed?
   const token = jwt.sign({ username }, SECRET_KEY);
-  console.log('#########################',token);
   return res.json({ token });
 })
 
