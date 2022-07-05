fdedc9b3a997bcdd0be08142b120d019786df9c7
Jason <jasoncmcvicar@gmail.com>
2022-07-04 20:00:05 -0500

    working through auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,29 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
 
 /** POST /login: {username, password} => {token} */
+router.post("/login", async function (req, res, next) {
+  const { username, password } = req.body;
+
+  const authResult = await authenticate(username, password);
+
+  if (authResult === true) {
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ token });
+  }
+
+  throw new UnauthorizedError("Invalid user/password");
+
+});
+
+// if (user) {
+//   if (await bcrypt.compare(password, user.password) === true) {
+//     const token = jwt.sign({ username }, SECRET_KEY);
+//     return res.json({ token });
+//   }
 
 
 /** POST /register: registers, logs in, and returns token.
